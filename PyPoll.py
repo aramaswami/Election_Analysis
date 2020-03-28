a4943a77fbbdfe524f93642b7d873beff3d6b26f
Ashok Ramaswami <aramaswami@gmail.com>
2020-03-28 16:33:09 -0700

    Added election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -12,15 +12,47 @@ file_to_load=os.path.join("Resources","election_results.csv")
 #load file for output using indirect file reference
 file_to_save=os.path.join("analysis","election_analysis.txt")
 
+#Initialize vote counter, candidate list, votes by candidate dictionary
+total_votes=0
+candidate_list=[]
+candidate_votes={}
+
+#Initialize values for winner, wining votes, and winning %
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #Open election results and read the file
 with open(file_to_load) as election_data:
 
 #use csv reader to read file that was opened; file_reader is a temp file in memory
     file_reader=csv.reader(election_data)
+#Exclude first row
     header=next(file_reader)
-    print(header)
 
+#Add total_votes, create candidate list, create candidate_votes dict
+    for row in file_reader:
+        total_votes=total_votes+1
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+            candidate_votes[row[2]]=0
+        candidate_votes[row[2]]+=1
 
-with open(file_to_save,"w") as election_analysis:
-    election_analysis.write("Counties in the election\n------------------------\nArapahoe\nDenver\nJefferson")
-
+    for candidate in candidate_votes:
+        votes=candidate_votes[candidate]
+        vote_percentage=int(votes)/int(total_votes)*100
+#Determine winner, winning votes, and winning%
+        if (votes>winning_count):
+            winning_count=votes
+            winning_percentage=vote_percentage
+            winning_candidate=candidate
+#Print each candiate with vote share and count
+        print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+#Print winning candidate and related stats
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
